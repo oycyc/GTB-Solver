d58c68940f4b379b10418efebf15f21d05974ed5
yangciou <me@yangciou.com>
2020-02-27 06:26:36 -0500

    merge test

--- assets/script.js
+++ assets/script.js
@@ -76,3 +76,12 @@ $(document).ready(function () {
   helpCloseBtn.addEventListener("click", function() {
     helpModal.style.display = "none";
   });
+
+
+// Character Count
+var count = document.getElementById("count")
+var input = document.getElementById("input")
+
+input.addEventListener("input", function(e){
+  count.innerHTML = e.target.value.length
+})
