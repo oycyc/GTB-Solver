ff50206c3a61d77dc0a920a3354c80464bb9a590
yangciou <me@yangciou.com>
2020-02-27 06:23:51 -0500

    js file merge test

--- charcount.js
+++
@@ -1,6 +0,0 @@
-var count = document.getElementById("count")
-var input = document.getElementById("input")
-
-input.addEventListener("input", function(e){
-  count.innerHTML = e.target.value.length
-})
